b98679576662f65d1a461b8cb891879e06792b20
itprogaur <professionalcodehub@gmail.com>
2022-02-24 22:44:31 +0530

    Added controllers

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function (req, res) {
+  return res.end("<h1>Welcome to Node Major Project</h1>");
+};
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = 8000;
 
-app.listen(port, err){
-    if(err){
-        console.log(`Error is running on the server: ${err}`);
-    }
-    else{
-        console.log(`Server is running on the port: ${port}`);
-    }
-}
+//use express router
+
+app.use("/", require("./routes"));
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error is running on the server: ${err}`);
+  } else {
+    console.log(`Server is running on the port: ${port}`);
+  }
+});
