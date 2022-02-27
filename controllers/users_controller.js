2f4380ce82df720f2cf28517f1946665137677ac
itprogaur <professionalcodehub@gmail.com>
2022-02-27 21:30:00 +0530

    Password encryption Added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,20 +1,11 @@
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/user");
 
 module.exports.profile = function (req, res) {
-  if (req.cookies.user_id) {
-    User.findById(req.cookies.user_id, function (err, user) {
-      if (user) {
-        return res.render("user_profile", {
-          title: "User Profile",
-          user: user,
-        });
-      } else {
-        return res.redirect("/users/sign-in");
-      }
-    });
-  } else {
-    return res.redirect("/users/sign-in");
-  }
+  return res.render("user_profile", {
+    title: "User Profile",
+  });
 };
 
 // render the sign up page
@@ -32,8 +23,13 @@ module.exports.signIn = function (req, res) {
 };
 
 // get the sign up data
-module.exports.create = function (req, res) {
-  if (req.body.password != req.body.confirm_password) {
+
+module.exports.create = async function (req, res) {
+  var salt = await bcrypt.genSalt(10);
+  var hashPassword = await bcrypt.hash(req.body.password, salt);
+  var confirm_hashpassword = await bcrypt.hash(req.body.confirm_password, salt);
+
+  if (hashPassword != confirm_hashpassword) {
     return res.redirect("back");
   }
 
@@ -60,30 +56,5 @@ module.exports.create = function (req, res) {
 
 // sign in and create a session for the user
 module.exports.createSession = function (req, res) {
-  // steps to authenticate
-  // find the user
-  User.findOne({ email: req.body.email }, function (err, user) {
-    if (err) {
-      console.log("error in finding user in signing in");
-      return;
-    }
-    // handle user found
-    if (user) {
-      // handle password which doesn't match
-      if (user.password != req.body.password) {
-        return res.redirect("back");
-      }
-
-      // handle session creation
-      res.cookie("user_id", user.id);
-      return res.redirect("/users/profile");
-    } else {
-      // handle user not found
-
-      return res.redirect("back");
-    }
-  });
+  return res.redirect("/");
 };
-
-
-module.exports.createSession
