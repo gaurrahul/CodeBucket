68c3181ffeb6c4d93141ae6e8dff6a6d17357ea3
itprogaur <professionalcodehub@gmail.com>
2022-03-16 20:06:31 +0530

    SCSS added

--- index.js
+++ index.js
@@ -8,11 +8,22 @@ const db = require("./config/mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
-const bcrypt = require("bcryptjs");
-
-//Mongo Session
 const MongoStore = require("connect-mongo");
-app.use(express.urlencoded());
+const mongodb = require("mongodb");
+const bodyParser = require("body-parser");
+const sassMilddleware = require("node-sass-middleware");
+
+app.use(
+  sassMilddleware({
+    src: "./assets/scss",
+    dest: "./assets/css",
+    debug: true,
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
+
+app.use(bodyParser.json());
 
 app.use(cookieParser());
 
@@ -27,7 +38,7 @@ app.set("layout extractScripts", true);
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-//mongo store is used to store the session cookie in the db
+// mongo store is used to store the session cookie in the db
 app.use(
   session({
     name: "codeial",
@@ -38,16 +49,15 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 100,
     },
-    store: MongoStore.create(
-      {
-        mongoUrl: "mongodb://localhost/codeial_development",
-        autoRemove: "disabled",
-      },
-
-      function (err) {
-        console.log(err || "Conect Mongo db");
-      }
-    ),
+    // store: new MongoStore(
+    //   {
+    //     mongooseConnection: db,
+    //     autoRemove: "disabled",
+    //   },
+    //   function (err) {
+    //     console.log(err || "connect-mongodb setup ok");
+    //   }
+    // ),
   })
 );
 
